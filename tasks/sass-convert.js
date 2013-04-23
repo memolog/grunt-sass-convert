c0f296b42a0297ba4ed97137552a466f93e766d1
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-04-23 13:48:34 +0900

    enable to use other command line options

--- tasks/sass-convert.js
+++ tasks/sass-convert.js
@@ -8,11 +8,11 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('sass-convert', 'just execute sass-convert', function() {
+  grunt.registerMultiTask('sass-convert', 'just execute sass-convert', function () {
     var exec = require('child_process').exec;
     var cb = this.async();
 
@@ -24,24 +24,29 @@ module.exports = function(grunt) {
 
 
     var files = this.files[0];
-    grunt.util.async.forEachSeries(files.src, function(src, next) {
-      var args = [
-        '--from', options['from'],
-        '--to', options['to'],
-        '--indent', options['indent']];
+    grunt.util.async.forEachSeries(files.src, function (src, next) {
+      var args = [];
+      grunt.util._.forOwn(options, function (value, key) {
+        args.push('--' + key, value);
+      })
       var out = src;
       args.push(src);
+
       if (files.dest) {
         out = files.dest + '/' + src.replace(/\.(css|scss|sass)/, '.' + options['to']);
       }
       var sass = grunt.util.spawn({
         cmd: 'sass-convert',
         args: args
-      }, function(error, result, code) {
-        if (result) {
-          grunt.file.write(out, result);
+      }, function (error, result, code) {
+        if (!error) {
+          if (result) {
+            grunt.file.write(out, result);
+          } else {
+            grunt.warn('got no result from ' + src);
+          }
         } else {
-          grunt.warn('got no result from '+src);
+          grunt.warn(error);
         }
         next(error);
       });
